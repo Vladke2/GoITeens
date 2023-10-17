b05ea0c36fd83a31ab741d9bd81c1a6ebdd4a24e
Vlad Sichkar <sickarvlad522@gmail.com>
2023-10-17 18:53:21 +0300

    home task

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def main():
     return 'This is a main page'
 
 
+@app.route('birthday')
+def main():
+    return '05.10.2008'
+
+
+@app.route('name')
+def main():
+    return 'Vlad'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
